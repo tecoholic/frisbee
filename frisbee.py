0fb508638e93f1265a1348187d43cfe15dac3390
tecoholic <aruntheguy@gmail.com>
2015-03-15 15:53:37 +0530

    added player contribution analysis function and tests

--- frisbee.py
+++ frisbee.py
@@ -1,4 +1,4 @@
-
+import re
 # Class definitions - to be used as data types
 
 class Player:
@@ -23,3 +23,38 @@ class Passes:
         self.string = pstr
         self.game_id = gid
         self.team_id = tid
+
+def analyse_game_string(gs):
+    passSeq = gs.split("\n")
+    players = []
+    act = re.compile("\*|\(P\)|\(F\)|\(S\)")
+    # create a list of unique player codes
+    for seq in passSeq:
+        hands = [act.sub('',s) for s in seq.split("-")]
+        for hand in hands:
+            if not hand in players and hand:
+                players.append(hand)
+    # Create a creds data store
+    playerCreds = []
+    for i in range(len(players)):
+        playerCreds.append(dict(zip(["catch", "drop", "throw", "snatch", "foul"], [0,0,0,0,0])))
+
+    # Parse each string and assign creds
+    for seq in passSeq:
+        hands = seq.split('-')
+        # get the credit value
+        for i,hand in enumerate(hands):
+            if re.search('\(S\)', hand):
+                playerCreds[players.index(act.sub('', hand))]["snatch"] += 1
+            elif i!=0:
+                playerCreds[players.index(act.sub('', hand))]["catch"] += 1
+
+            if re.search('\*', hand):
+                playerCreds[players.index(act.sub('', hand))]["drop"] += 1
+            elif re.search('\(F\)', hand):
+                playerCreds[players.index(act.sub('', hand))]["foul"] += 1
+            elif not re.search('\(P\)', hand):
+                playerCreds[players.index(act.sub('', hand))]["throw"] += 1
+    return dict(zip(players, playerCreds))
+
+
---
+++ test/test_frisbee.py
@@ -0,0 +1,73 @@
+import unittest
+
+from frisbee import *
+
+class AnalysisTestCase(unittest.TestCase):
+    """Tests all the analysis functions"""
+    def __get_cred_dict(self, catch, drop, throw, snatch, foul):
+        """Returns a dictionary of player creds as expected from analysis"""
+        return dict(zip(["catch", "drop", "throw", "snatch", "foul"],
+            [catch, drop, throw, snatch, foul]))
+
+    def test_for_1player_drop(self):
+        """Test for 1 player 1 drop"""
+        # 1 player drop
+        gs = "PL1*"
+        res = {"PL1" : self.__get_cred_dict(0,1,0,0,0)}
+        self.assertDictEqual(analyse_game_string(gs), res)
+
+    def test_for_2player_drop(self):
+        """Test for 2 players 1 throw 1 catch and 1 drop"""
+        # 2 player drop
+        gs = "PL1-PL2*"
+        res = {"PL1" : self.__get_cred_dict(0,0,1,0,0),
+                "PL2" : self.__get_cred_dict(1,1,0,0,0)}
+        self.assertDictEqual(analyse_game_string(gs), res)
+
+    def test_for_throw_catch_drop(self):
+        """Test for multiple passes and a drop"""
+        # Sucessful pass
+        gs = "PL1-PL2-PL3*"
+        res = {"PL1" : self.__get_cred_dict(0,0,1,0,0),
+                "PL2" : self.__get_cred_dict(1,0,1,0,0),
+                "PL3" : self.__get_cred_dict(1,1,0,0,0)}
+        self.assertDictEqual(analyse_game_string(gs), res)
+
+    def test_for_repeatitive_pass(self):
+        """Test for with repeatitive pass to the same player"""
+        gs = "SPF1-SPF2-SPF1-SPF2-SPF1*"
+        res = {"SPF1" : self.__get_cred_dict(2,1,2,0,0),
+                "SPF2": self.__get_cred_dict(2,0,2,0,0)}
+        self.assertDictEqual(analyse_game_string(gs), res)
+
+    def test_for_snatch(self):
+        """Test for accounting snatch with a sucessful pass"""
+        gs = "PL1(S)-PL2-PL3*"
+        res = {"PL1": self.__get_cred_dict(0,0,1,1,0),
+                "PL2": self.__get_cred_dict(1,0,1,0,0),
+                "PL3": self.__get_cred_dict(1,1,0,0,0)}
+        self.assertDictEqual(analyse_game_string(gs), res)
+
+    def test_for_snatch_drop(self):
+        """Test for accounting snatch that was dropped"""
+        gs = "PL1(S)*"
+        res = {"PL1": self.__get_cred_dict(0,1,0,1,0)}
+        self.assertDictEqual(analyse_game_string(gs), res)
+
+    def test_for_foul(self):
+        """Test for counting fouls"""
+        gs = "PL1-PL2-PL3(F)"
+        res = {"PL1": self.__get_cred_dict(0,0,1,0,0),
+                "PL2": self.__get_cred_dict(1,0,1,0,0),
+                "PL3": self.__get_cred_dict(1,0,0,0,1)}
+        self.assertDictEqual(analyse_game_string(gs), res)
+
+    def test_for_ignore_points(self):
+        """Test for the player analysis to ignore the points"""
+        gs = "PL1-PL2(P)"
+        res = {"PL1" : self.__get_cred_dict(0,0,1,0,0),
+                "PL2" : self.__get_cred_dict(1,0,0,0,0)}
+        self.assertDictEqual(analyse_game_string(gs), res)
+
+if __name__ == "__main__":
+    unittest.main()
